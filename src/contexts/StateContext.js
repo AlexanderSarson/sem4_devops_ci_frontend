fedd81bd85b52d3f62787d6fe89031bc962eebc3
Alexander Sarson <Alexander_Sarson@Outlook.com>
2020-10-09 09:17:55 +0200

    updated

--- src/contexts/StateContext.js
+++ src/contexts/StateContext.js
@@ -1,17 +1,17 @@
-import React, { useReducer, createContext } from "react";
-import id from "uuid/v4";
+import React, { useReducer, createContext } from 'react';
+import id from 'uuid/v4';
 
 export const StateContext = createContext();
 
-const STATE_ADD = "STATE_ADD";
-const STATE_REMOVE = "STATE_REMOVE";
-const STATE_UNDO = "STATE_UNDO";
-const STATE_REDO = "STATE_REDO";
+const STATE_ADD = 'STATE_ADD';
+const STATE_REMOVE = 'STATE_REMOVE';
+const STATE_UNDO = 'STATE_UNDO';
+const STATE_REDO = 'STATE_REDO';
 
 const initialState = [
-  { id: id(), name: "test1", age: 20, email: "hej@hej1.dk" },
-  { id: id(), name: "test2", age: 21, email: "hej@hej2.dk" },
-  { id: id(), name: "test3", age: 22, email: "hej@hej3.dk" }
+  { id: id(), name: 'test1', age: 20, email: 'hej@hej1.dk' },
+  { id: id(), name: 'test2', age: 21, email: 'hej@hej2.dk' },
+  { id: id(), name: 'test3', age: 22, email: 'hej@hej3.dk' }
 ];
 
 const defaultState = {
@@ -38,7 +38,7 @@ const reducer = (state = defaultState, action) => {
 
   if (action.type === STATE_REMOVE) {
     const newPresent = state.present.filter(
-      obj => obj.id !== action.payload.id
+      (obj) => obj.id !== action.payload.id
     );
     return {
       past: [state.present, ...state.past],
@@ -73,16 +73,14 @@ export const StateProvider = ({ children }) => {
   const isPast = !!state.past.length;
   const isFuture = !!state.future.length;
 
-  const addState = stateObject => {
+  const addState = (stateObject) => {
     dispatch({
       type: STATE_ADD,
-      payload: {
-        ...stateObject
-      }
+      payload: { ...stateObject }
     });
   };
 
-  const RemoveState = id => {
+  const RemoveState = (id) => {
     dispatch({
       type: STATE_REMOVE,
       payload: {
